b15e8083d281e1e9da0467869fb4872c1f04eb8f
shaheensms <shaheensms111@gmail.com>
2021-01-31 02:01:26 +0600

    Initial commit

---
+++ app/src/main/java/com/metacoders/e_proshashonadmin/AdminCreateActivity.java
@@ -0,0 +1,14 @@
+package com.metacoders.e_proshashonadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_create);
+    }
+}
