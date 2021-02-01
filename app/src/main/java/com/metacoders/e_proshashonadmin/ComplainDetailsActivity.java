7ee2cf1c02ecc48201fe461fef84763703604ce5
shaheensms <shaheensms111@gmail.com>
2021-02-02 01:51:50 +0600

    Admin design

---
+++ app/src/main/java/com/metacoders/e_proshashonadmin/ComplainDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.metacoders.e_proshashonadmin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ComplainDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complain_details);
+    }
+}
